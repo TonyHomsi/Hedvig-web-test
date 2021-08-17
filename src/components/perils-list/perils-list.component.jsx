93b96d3236e5e003d5407687e815d4e84bfee31c
TonyHomsi <tonyhomsi87@gmail.com>
2021-08-17 15:08:38 +0200

    Changing ClassName in the Peril-list component

--- src/components/perils-list/perils-list.component.jsx
+++ src/components/perils-list/perils-list.component.jsx
@@ -4,7 +4,7 @@ import {Peril} from '../peril/peril.component'
 import "./perils-list.style.css";
 
 export const PerilList = ({perils = [], onItemClick}) => (
-  <div className="css-dsn619">
+  <div className="peril-list">
     {perils.map((peril, idx) => (
       <Peril key={`${idx}-${peril.title}`} peril={peril} onClick={() => onItemClick(peril)}/>
     ))}
